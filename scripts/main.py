2f1acd80d907d28be9d8da4ad462262bec1a0000
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2023-12-22 12:27:08 -0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,3 +5,10 @@ from selenium.webdriver.chrome.service import Service
 servico = Service(ChromeDriverManager().install())
 
 navegador = webdriver.Chrome(service=servico)
+
+#funcao para realizar login no facebook (necessario 2 fatores talvez)
+#Loop para verificar se é necessário fazer algum post
+#funcao para adicionar grupos do facebook no supabase
+#funcao para procurar novos grupos  no facebook
+#funcao para postar anuncios nos grupos do facebook
+
